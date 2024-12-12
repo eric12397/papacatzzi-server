256c88a7cfea7be2e2bd9c6ad26b162ce9a81a17
Eric <eric12397@gmail.com>
2024-12-12 11:00:15 -0500

    adding cors middleware

--- api/server.go
+++ api/server.go
@@ -27,9 +27,26 @@ func (s *Server) setupRouter() (r *mux.Router) {
 	r = mux.NewRouter()
 
 	r.HandleFunc("/posts", s.getPosts).Methods("GET")
+	r.Use(corsMiddleware)
 	return
 }
 
 func (s *Server) ListenAndServe() {
 	log.Fatal(s.server.ListenAndServe())
+}
+
+func corsMiddleware(next http.Handler) http.Handler {
+	return http.HandlerFunc(func(w http.ResponseWriter, r *http.Request) {
+		w.Header().Add("Access-Control-Allow-Origin", "*")
+		w.Header().Add("Access-Control-Allow-Credentials", "true")
+		w.Header().Add("Access-Control-Allow-Headers", "Content-Type, Content-Length, Accept-Encoding, X-CSRF-Token, Authorization, accept, origin, Cache-Control, X-Requested-With")
+		w.Header().Add("Access-Control-Allow-Methods", "POST, GET, OPTIONS, PUT, DELETE")
+
+		if r.Method == "OPTIONS" {
+			http.Error(w, "No Content", http.StatusNoContent)
+			return
+		}
+
+		next.ServeHTTP(w, r)
+	})
 }
