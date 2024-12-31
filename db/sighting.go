08396674c13c46b39141f9e67d2d9e4d9e109d0c
Eric <eric12397@gmail.com>
2024-12-31 11:42:41 -0500

    fetching timestamp for sighting details db method

--- db/sighting.go
+++ db/sighting.go
@@ -37,10 +37,10 @@ func (s Store) GetSightingsByCoordinates(
 func (s Store) GetSightingByID(id string) (sighting models.Sighting, err error) {
 
 	err = s.db.QueryRow(`
-		SELECT id, user_id, animal_type
+		SELECT id, user_id, animal_type, photo_url, description, created_at
 		FROM sightings
 		WHERE id = $1
-	`, id).Scan(&sighting.ID, &sighting.Reporter, &sighting.Animal)
+	`, id).Scan(&sighting.ID, &sighting.Reporter, &sighting.Animal, &sighting.PhotoURL, &sighting.Description, &sighting.Timestamp)
 
 	return
 }
