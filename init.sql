9e45e39fd5eaf5d4162d67e30d113afb778c49e6
Eric <eric12397@gmail.com>
2025-01-03 23:12:23 -0500

    creating users table

--- init.sql
+++ init.sql
@@ -9,11 +9,13 @@ CREATE TABLE sightings (
     created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
 );
 
-INSERT INTO sightings (user_id, animal_type, latitude, longitude) VALUES 
-    ('Johnny Park', 'Dog', 39.945147922934126, -75.14459310653145),
-    ('David Robinson', 'Cat', 39.945752660141075, -75.144704423162),
-    ('Brandon Gomez', 'Cat', 39.94562419967605, -75.14516242278123),
-    ('Doug Lee', 'Cat', 39.945727657063856, -75.14653630166342);
+CREATE TABLE users (
+    id SERIAL PRIMARY KEY,
+    username TEXT NOT NULL,
+    email TEXT NOT NULL,
+    password TEXT NOT NULL,
+    created_at TIMESTAMP DEFAULT CURRENT_TIMESTAMP
+);
 
 -- Create a spatial index for efficient querying
 CREATE INDEX idx_sightings_coordinates ON sightings USING GIST (
